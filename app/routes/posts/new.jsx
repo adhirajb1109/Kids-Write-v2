76a5fc3ddb4262199d29a66c1a20ec8efacd30ba
Adhiraj Bhatia <adhirajbhatia@pop-os.localdomain>
2022-03-12 11:49:44 +0530

    Changed Database Hosting Provider From Heroku To Railway

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,4 +1,4 @@
-import { Link, redirect } from "remix";
+import { redirect } from "remix";
 import { db } from "~/utils/db.server";
 import { getUser } from "~/utils/session.server";
 export const loader = async ({ request }) => {
@@ -31,7 +31,7 @@ function NewPost() {
                         <label htmlFor="body">Body :</label>
                         <textarea name="body" id="body" rows="10" required></textarea>
                     </div>
-                   <button type="submit" className="btn btn-block">
+                    <button type="submit" className="btn btn-block">
                         Add <i class="fas fa-plus icon"></i>
                     </button>
                 </form>
